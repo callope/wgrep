438fb3b5b61a7937c3ea40d97f68e78203ea5b6f
callope <callope@proton.me>
2023-01-14 17:29:59 -0300

    refactoring everything to make a library
    this code is gonna be usable in other programs as well as
    a command line tool.

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from . import output
+from .output import (
+    as_json, as_xml, as_toml
+)
---
+++ src/output.py
@@ -0,0 +1,13 @@
+import whois
+
+import json
+import tomlkit
+
+def as_json(data):
+    ...
+
+def as_xml(data):
+    ...
+
+def as_toml(data):
+    ...
