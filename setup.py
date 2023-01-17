6b75c3a4fb03620fdf861bd3f984f9ecfc358cfc
callope <callope@proton.me>
2023-01-16 22:29:38 -0300

    updating dependencies
    requirements.txt is not going to be installed when installing the package with pip

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '1.0.0'
 DESCRIPTION = 'Parse whois data into program-readable formats (like JSON, XML, TOML, etc.)'
 LONG_DESCRIPTION = """A package that allows you to convert whois data to computer-readable data formats (like JSON, XML, etc.)
 It is convenient to make lookup programs or API integrations."""
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['python-whois'],
+    install_requires=['python-whois', 'toml', 'dict2xml'],
     keywords=['python', 'whois', 'lookup', 'api integration', 'information'],
     classifiers=[
         "Development Status :: 1 - Planning",
